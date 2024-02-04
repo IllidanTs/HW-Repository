41eb3362a70e9c2d0077ee72dd2a3c4a0547de4b
Elias Tsatsorin <itsatsorin@gmail.com>
2024-02-04 16:57:05 -0500

    Скільки тобі років ? 4.0

--- HW4.js
+++ HW4.js
@@ -11,7 +11,7 @@ if (isNaN(age)) {
     const lastDigit = age % 10;
       if (age !== 11 && lastDigit === 1) {
           word = "рік";
-      } else if (age > 4 && age <= 21) {
+      } else if (age > 4 && age < 21) {
           word = 'років';
       } else if (lastDigit > 1 && lastDigit < 5) {
           word = 'роки';
