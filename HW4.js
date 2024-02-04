2be290345d15e39ef6b32d6981b3b1debe488e22
Elias Tsatsorin <itsatsorin@gmail.com>
2024-02-04 16:25:49 -0500

    Скільки тобі років ? 3.0

--- HW4.js
+++ HW4.js
@@ -1,22 +1,23 @@
 let age = prompt("Скільки тобі років?");
 console.log(typeof age);
 if (isNaN(age)) {
-    alert("Введено не число");
+  alert("Введено не число");
 } else {
-    age = Number(age);
-    if (age < 0 || age > 100) {
-        alert("Введено некоректний вік");
-    } else {
-        let word;
-        const lastDigit = age % 10;
-        const lastTwoDigits = age % 100;
-        if (age === 1 || (lastTwoDigits >= 11 && lastTwoDigits <= 14)) {
-            word = 'рік';
-        } else if (lastDigit >= 2 && lastDigit <= 4) {
-            word = 'роки';
-        } else {
-            word = 'років';
-        }
-        alert(age + " " + word);
-    }
+  age = Number(age);
+  if (age < 0 || age > 100) {
+    alert("Введено некоректний вік");
+  } else {
+    let word;
+    const lastDigit = age % 10;
+      if (age !== 11 && lastDigit === 1) {
+          word = "рік";
+      } else if (age > 4 && age <= 21) {
+          word = 'років';
+      } else if (lastDigit > 1 && lastDigit < 5) {
+          word = 'роки';
+      } else {
+          word = 'років';
+      }
+    alert(age + " " + word);
+  }
 }
