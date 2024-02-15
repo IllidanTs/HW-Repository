cd27dc27441d8a6a17f3c323821ccf24f162724a
Elias Tsatsorin <itsatsorin@gmail.com>
2024-02-15 12:25:18 -0500

    Commit 2.0

--- HW7Task2.js
+++ HW7Task2.js
@@ -1,59 +1,67 @@
 let services = {
-  "Стрижка": 60,
-  "Гоління": 80,
-  "Миття голови": 100,
+  "Стрижка": "60 грн",
+  "Гоління": "80 грн",
+  "Миття голови": "100 грн",
 
   addService: function(name, price) {
-    this[name] = price;
+    this[name] = price + ' грн';
   },
 
   minPrice: function() {
     let min;
     for (let service in this) {
-      if (typeof this[service] === 'number') {
-        if (!min) {
-          min = this[service];
-          continue;
-        }
-
-        if (this[service] < min) {
-          min = this[service];
+      if (typeof this[service] === 'string') {
+        let price = parseInt(this[service]);
+        if (!isNaN(price)) {
+          if (!min) {
+            min = price;
+            continue;
+          }
+          if (price < min) {
+            min = price;
+          }
         }
       }
     }
-    return min;
+    return min + ' грн';
   },
 
   maxPrice: function() {
     let max;
     for (let service in this) {
-      if (typeof this[service] === 'number') {
-        if (!max) {
-          max = this[service];
-          continue;
-        }
-        if (this[service] > max) {
-          max = this[service];
+      if (typeof this[service] === 'string') {
+        let price = parseInt(this[service]);
+        if (!isNaN(price)) {
+          if (!max) {
+            max = price;
+            continue;
+          }
+          if (price > max) {
+            max = price;
+          }
         }
       }
     }
-    return max;
+    return max + ' грн';
   },
 
   calculatePrices: function() {
     let totalPrice = 0;
     for (let service in this) {
-      if (typeof this[service] === 'number') {
-        totalPrice += this[service];
+      if (typeof this[service] === 'string') {
+        let price = parseInt(this[service]);
+        if (!isNaN(price)) {
+          totalPrice += price;
+        }
       }
     }
-    return totalPrice;
+    return totalPrice + ' грн';
   },
 };
 
-services.addService('Фарбування волосся', 300);
-services.addService('Розбити обличчя майстру', 500);
+services.addService('Фарбування волосся', '300 грн');
+services.addService('Розбити обличчя майстру', '500 грн');
 
-console.log('Загальна вартість наданих послуг:', services.calculatePrices(), 'грн');
-console.log('Мінімальна вартість:', services.minPrice(), 'грн');
-console.log('Максимальна вартість:', services.maxPrice(), 'грн');
+console.log('Загальна вартість наданих послуг:', services.calculatePrices());
+console.log('Мінімальна вартість:', services.minPrice());
+console.log('Максимальна вартість:', services.maxPrice());
